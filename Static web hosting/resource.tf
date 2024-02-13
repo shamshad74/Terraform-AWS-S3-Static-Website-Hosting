6b47dedcada13ff6b1edc15179f92f6a72588b15
shamshad <shamsad004sane@gmail.com>
2024-02-13 13:07:14 +0530

    sham

--- Static web hosting/resource.tf
+++ Static web hosting/resource.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "bucket1" {
-    bucket = "web-bucket-mathesh"
+    bucket = "web-bucket-shamshad"
   
 }
 resource "aws_s3_bucket_public_access_block" "bucket1" {
@@ -12,14 +12,14 @@ resource "aws_s3_bucket_public_access_block" "bucket1" {
 }
 
 resource "aws_s3_object" "index" {
-  bucket = "web-bucket-mathesh"
+  bucket = "web-bucket-shamshad"
   key    = "index.html"
   source = "index.html"
   content_type = "text/html"
 }
 
 resource "aws_s3_object" "error" {
-  bucket = "web-bucket-mathesh"
+  bucket = "web-bucket-shamshad"
   key    = "error.html"
   source = "error.html"
   content_type = "text/html"
